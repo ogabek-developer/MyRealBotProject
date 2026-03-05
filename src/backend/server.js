44d621d64ba50a7aecd126a9015940202fd473ad
cyber-dev1 <ogabek200825@gmail.com>
2026-03-05 15:08:58 +0500

    Fixed

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,16 +2,16 @@ import app from './app.js';
 import sequelize from './config/db.js';
 import bot from '../bot/bot.js';
 
-const PORT = process.env.PORT || 3000;
 
 (async () => {
   try {
-
+    
     await sequelize.authenticate();
     console.log('Database connected!');
-
+    
     await sequelize.sync({ alter: true });
-
+    
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is running on port: ${PORT}`);
     });
