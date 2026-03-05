11af90d9ed053dcd1ad5ae744c16ec2c5d7e51dc
cyber-dev1 <ogabek200825@gmail.com>
2026-03-05 16:57:09 +0500

    Upd

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -3,15 +3,18 @@ import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
 config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  // SSL sertifikatni qabul qilish
+    }
+  },
+  logging: false,
+});
 
 export default sequelize;
