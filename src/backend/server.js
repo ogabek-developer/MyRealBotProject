7f0796524000c6e726e70935b9c6eb454750a9a6
cyber-dev1 <ogabek200825@gmail.com>
2026-03-05 15:25:51 +0500

    Fixed

--- src/backend/server.js
+++ src/backend/server.js
@@ -12,11 +12,10 @@ import bot from '../bot/bot.js';
     await sequelize.sync({ alter: true });
     
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    app.listen(PORT ? PORT : 5000, () => {
       console.log(`Server is running on port: ${PORT}`);
     });
 
-    await bot.launch();   // 🔥 SHU YERDA
     console.log('Bot is running...');
 
   } catch (err) {
